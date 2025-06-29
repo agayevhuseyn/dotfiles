837c93772c7dc718908726984146125a037428e1
Hüseyn Ağayev <huseynaga269@gmail.com>
2025-06-29 11:25:47 +0400

    create dir if does not exist

--- bash-files/screenshot.sh
+++ bash-files/screenshot.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
 
-SAVE_DIR="/home/huseyn/Pictures/Screenshots"
+SAVE_DIR="/home/$(whoami)/Pictures/Screenshots"
 
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+
+if [ ! -d $SAVE_DIR ]; then
+    mkdir -p $SAVE_DIR
+fi
 
 gnome-screenshot -f $SAVE_DIR/SCR_$DATE.png
