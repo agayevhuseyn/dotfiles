e71ab67907c1532bd55017af8de914c0a07b13e4
Huseyn Aghayev <huseyn@seallang.org>
2025-06-29 20:24:34 +0400

    remove comment

--- bash-files/yt-video.sh
+++ bash-files/yt-video.sh
@@ -4,5 +4,5 @@ if [ -z "$1" ]; then
 	echo "Enter the URL"
 	exit 1
 fi
-# yt-dlp -f 'bestvideo+bestaudio' $1
+
 yt-dlp -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' -o "%(title)s.%(ext)s" $1
